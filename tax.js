bdd84f821e11349e7d5a721527b950088522810e
Darcy <sharks@grapevine.com.au>
2016-05-23 19:42:12 +1000

    Update tax.js
    Finished the if statement in calcTax()

--- tax.js
+++ tax.js
@@ -11,4 +11,26 @@ function calcTax()
 
 	if (income < 0)
 		output.innerHTML = MSG_INVALID;
+	else if (income <= 16000)
+		output.innerHTML = MSG_TAX + tax;
+	else if (income <= 52500)
+	{
+		tax = (income - 16000) * 0.1;
+		output.innerHTML = MSG_TAX + tax;
+	}
+	else if (income <= 113000)
+	{
+		tax = 3650 + (income - 52500) * 0.16;
+		output.innerHTML = MSG_TAX + tax;
+	}
+	else if (income <= 184500)
+	{
+		tax = 13330 + (income - 113000) * 0.24;
+		output.innerHTML = MSG_TAX + tax;
+	}
+	else
+	{
+		tax = 30490 + (income - 184500) * 0.36;
+		output.innerHTML = MSG_TAX + tax;
+	}
 }
