770748b015525ad0602141b5a7c48a8992a17768
Darcy <sharks@grapevine.com.au>
2016-05-31 12:01:07 +1000

    Separated the calcTax function into two functions

--- tax.js
+++ tax.js
@@ -2,35 +2,34 @@ var input = document.getElementById("input");
 var output = document.getElementById("output");
 
 var MSG_TAX;
-var MSG_INVALID = "The number you have entered is invalid.";
+var MSG_INVALID = "The income you have entered is invalid.";
 
-function calcTax()
+function calcTax(x)
+{
+	var tax;
+
+	if (x < 0)
+		return null;
+	else if (x <= 16000)
+		tax = 0;
+	else if (x <= 52500)
+		tax = (x - 16000) * 0.1;
+	else if (x <= 113000)
+		tax = 3650 + (x - 52500) * 0.16;
+	else if (x <= 184500)
+		tax = 13330 + (x - 113000) * 0.24;
+	else
+		tax = 30490 + (x - 184500) * 0.36;
+	
+	return tax;
+}
+
+function taxIo()
 {
 	var income = input.value;
-	var tax = 0;
-
-	if (income < 0)
+	
+	if (calcTax(income) == -1)
 		output.innerHTML = MSG_INVALID;
-	else if (income <= 16000)
-		output.innerHTML = MSG_TAX + tax;
-	else if (income <= 52500)
-	{
-		tax = (income - 16000) * 0.1;
-		output.innerHTML = MSG_TAX + tax;
-	}
-	else if (income <= 113000)
-	{
-		tax = 3650 + (income - 52500) * 0.16;
-		output.innerHTML = MSG_TAX + tax;
-	}
-	else if (income <= 184500)
-	{
-		tax = 13330 + (income - 113000) * 0.24;
-		output.innerHTML = MSG_TAX + tax;
-	}
 	else
-	{
-		tax = 30490 + (income - 184500) * 0.36;
-		output.innerHTML = MSG_TAX + tax;
-	}
+		output.innerHTML = MSG_TAX + calcTax(income);
 }
