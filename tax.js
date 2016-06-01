23ab2383c71e138852ae93f8680293eabbb3ca6d
Darcy <sharks@grapevine.com.au>
2016-06-01 13:38:49 +1000

    Update tax.js

--- tax.js
+++ tax.js
@@ -2,15 +2,12 @@ var input = document.getElementById("input");
 var output = document.getElementById("output");
 
 var MSG_TAX;
-var MSG_INVALID = "The income you have entered is invalid.";
 
 function calcTax(x)
 {
 	var tax;
 
-	if (x < 0)
-		return null;
-	else if (x <= 16000)
+	if (x <= 16000)
 		tax = 0;
 	else if (x <= 52500)
 		tax = (x - 16000) * 0.1;
@@ -33,8 +30,5 @@ function taxIo()
 {
 	var income = input.value;
 	
-	if (calcTax(income) == -1)
-		output.innerHTML = MSG_INVALID;
-	else
-		output.innerHTML = MSG_TAX + calcTax(income);
+	output.innerHTML = MSG_TAX + calcTax(income);
 }
